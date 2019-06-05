e49ef404cdbe19a510c61d35c1c8ccde049c2513
roalxDcar <roalmollericona@gmail.com>
2019-06-05 10:22:47 -0400

    nuevo

---
+++ database/migrations/2019_06_04_231313_create_student_fees_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentfeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_fees', function (Blueprint $table) {
+            
+            $table->increments('idstudent_fee');
+            $table->integer('id_student')->unsigned();
+            $table->integer('id_fee_types')->unsigned();
+
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('description',50);
+            $table->decimal('price',7,2);
+            $table->boolean('state')->default(1);
+            $table->timestamps();
+
+            $table->foreign('id_student')->references('iduser')->on('users');
+            $table->foreign('id_fee_types')->references('idfee_types')->on('fee_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_fees');
+    }
+}
